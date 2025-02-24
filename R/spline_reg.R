2e152dede235c8e56541e37d01c9603dea545247
kybak90 <kybak@sungshin.ac.kr>
2025-02-24 22:42:45 +0900

    add README

--- R/spline_reg.R
+++ R/spline_reg.R
@@ -38,7 +38,22 @@ add_boundary_knots = function(x, interior_knots, degree = 3, tiny = 1e-5)
   return(knots)
 }
 
+#' Generating interior knots sequence for given data points
+#'
+#' This function computes a knot sequence based on the quantiles of the data points
+#'
+#' @param x Numeric vector representing data points
+#' @param dimension Numeric value indicating the number of basis functions
+#' @param degree Numeric value representing the spline degree
+#'
+#' @return Numeric vector with interior knots sequence
+#'
 #' @export
+#'
+#' @examples
+#' x = runif(50, 0, 1)
+#' knots = knots_quantile(x, 5, 3)
+#' print(knots)
 knots_quantile = function(x, dimension, degree = 3)
 {
   dimension = max(dimension, degree + 1)
@@ -52,6 +67,30 @@ knots_quantile = function(x, dimension, degree = 3)
   return(interior_knots)
 }
 
+#' Fitting regression spline estimator
+#'
+#' This function computes the coefficients spline regression estimator.
+#'
+#' @param x_values,y_values Numeric vectors
+#' @param interior_knots Numeric vector representing interior knots sequence. It can be obtained from \code{\link{knots_quantile}} function based on the given x points.
+#' @param degree Numeric value indicating the spline degree
+#'
+#' @return List containing the spline coefficients estimates, knots sequence, spline degree
+#'
+#' @examples
+#' set.seed(923)
+#' n = 30
+#' x_values = sort(runif(n, 0, 1))
+#' y_values = sin(2 * pi * x_values) + cos(4 * pi * x_values) + rnorm(n, sd = 0.2)
+#' #spline degree specification
+#' degree = 3
+#' # knot generation
+#' num_interior_knots = 5
+#' interior_knots = knots_quantile(x_values, num_interior_knots)
+#' # model fitting
+#' model = fit_spline(x_values, y_values, interior_knots, degree)
+#' print(model)
+#'
 #' @export
 fit_spline = function(x_values, y_values, interior_knots, degree)
 {
@@ -61,6 +100,16 @@ fit_spline = function(x_values, y_values, interior_knots, degree)
   return(list(beta = beta, knots = knots, degree = degree))
 }
 
+#' Predicting the values of y for given x based on the spline estimator
+#'
+#' This function computes the predicted values of y for given x based on the spline estimator.
+#'
+#' @param model List object obtained from the \code{\link{fit_spline}} function
+#'
+#' @param new_x Numeric vector representing a grid of evaluation points
+#'
+#' @return Numeric vector with predicted values at new_x
+#'
 #' @export
 predict_spline = function(model, new_x)
 {
@@ -69,6 +118,14 @@ predict_spline = function(model, new_x)
   return(y_pred)
 }
 
+#' Plotting spline estimator with scatter plots
+#'
+#' This function provides a plot of the spline estimator with data points
+#'
+#' @param x_values,y_values Numeric vector
+#' @param model List object obtained from the \code{\link{fit_spline}} function
+#' @param grid_x Numeric vector with a grid of evaluation points
+#'
 #' @export
 #' @import ggplot2
 plot_spline = function(x_values, y_values, model, grid_x)
